8ea0c876afba3b21d0b2ad073746c6710d10a54c
Hardik Yagnik <hardik.yagnik@accenture.com>
2024-03-24 01:05:50 +0800

    Fmt terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,14 @@ provider "aws" {
 }
 
 module "backend" {
-  source                                              = "./modules/remote_backend"
-  iam_user_name                                       = var.iam_user_name
-  bucket_name                                         = var.bucket_name
-  table_name                                          = var.table_name
+  source        = "./modules/remote_backend"
+  iam_user_name = var.iam_user_name
+  bucket_name   = var.bucket_name
+  table_name    = var.table_name
 }
 
 output "iam_user_name" {
-    value = module.backend.iam_user_name
+  value = module.backend.iam_user_name
 }
 
 /* Commented out
