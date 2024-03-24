6a872db41c560aba8f55cebaadea7bbc84a40928
Hardik Yagnik <hardik.yagnik@accenture.com>
2024-03-24 13:10:00 +0800

    Mesages 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ provider "aws" {
 
 module "backend" {
   source = "./modules/remote_backend"
-  #iam_user_name = var.iam_user_name
   bucket_name = var.bucket_name
   table_name  = var.table_name
 }
